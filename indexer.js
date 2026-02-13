72cc8459b4ff4a56f25dcde6fdf2c6166e90202c
VJakoby <jakobwango@protonmail.com>
2026-02-13 15:50:33 +0100

    Changed User-Agent

--- indexer.js
+++ indexer.js
@@ -293,7 +293,7 @@ class ContentIndexer {
             const response = await axios.get(url, {
                 timeout,
                 headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+                    'User-Agent': 'PKBI',
                     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                     'Accept-Language': 'en-US,en;q=0.5',
                 },
